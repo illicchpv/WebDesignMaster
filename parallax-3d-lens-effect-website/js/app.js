9d98354c4aaa6d35234bc61ebe300639f3b08c24
illicchpv <illich58@mail.ru>
2023-06-03 19:05:50 +0300

    corr+

--- parallax-3d-lens-effect-website/js/app.js
+++ parallax-3d-lens-effect-website/js/app.js
@@ -1,9 +1,15 @@
 document.addEventListener('mousemove', e => {
-  // document.documentElement - <html style...
-  Object.assign(document.documentElement, {
-    style: `
-    --move-x:${(e.clientX - window.innerWidth / 2)*-0.005}deg;
-    --move-y:${(e.clientY - window.innerHeight / 2)*-0.01}deg;
-    `
-  })
+  // // document.documentElement - <html style...
+  // можно так
+  // Object.assign(document.documentElement, {
+  //   style: `
+  //   --move-x:${(e.clientX - window.innerWidth / 2)*-0.005}deg;
+  //   --move-y:${(e.clientY - window.innerHeight / 2)*-0.01}deg;
+  //   `
+  // })
+  // можно так
+  document.body.style.cssText += `
+  --move-x:${(e.clientX - window.innerWidth / 2)*-0.005}deg;
+  --move-y:${(e.clientY - window.innerHeight / 2)*-0.01}deg;
+  `
 })
